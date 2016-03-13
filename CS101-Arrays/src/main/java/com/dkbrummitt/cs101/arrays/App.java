b67b6cc602cf3339c7844fd2fdfa6eb530b0a4e7
Delicia Brummitt <delicia@Delicias-MacBook-Pro.local>
2016-03-13 10:18:56 -0400

    Initial project structure- sans code

---
+++ CS101-Arrays/src/main/java/com/dkbrummitt/cs101/arrays/App.java
@@ -0,0 +1,13 @@
+package com.dkbrummitt.cs101.arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ CS101-Utils/src/main/java/com/dkbrummitt/cs101/utils/App.java
@@ -0,0 +1,13 @@
+package com.dkbrummitt.cs101.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
